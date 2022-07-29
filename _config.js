8ae0f0507bcd4bb86337a8139420441ecaea71ff
Koigi <wakoigi@gmail.com>
2022-07-29 14:40:27 +0300

    add mongo pwd

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://sprintech:sprintech@sprintech.qf4jkk2.mongodb.net/?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://sprintech:sprintech@sprintech.qf4jkk2.mongodb.net/?retryWrites=true&w=majority',
+    production: 'mongodb+srv://wakoigi:<C0lombus01@>@cluster0.oyjbt.mongodb.net/?retryWrites=true&w=majority',
+    development: 'mongodb+srv://wakoigi:<C0lombus01@>@cluster0.oyjbt.mongodb.net/?retryWrites=true&w=majority',
+    test: 'mongodb+srv://wakoigi:<password>@cluster0.oyjbt.mongodb.net/?retryWrites=true&w=majority',
 }
 module.exports = config;
