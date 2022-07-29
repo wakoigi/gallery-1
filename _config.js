4113ca339eee7036e30ce2ec5c8d8e45701ea83c
Koigi <wakoigi@gmail.com>
2022-07-29 14:50:14 +0300

    add mongo pwd2

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://wakoigi:<C0lombus01@>@cluster0.oyjbt.mongodb.net/?retryWrites=true&w=majority',
-    development: 'mongodb+srv://wakoigi:<C0lombus01@>@cluster0.oyjbt.mongodb.net/?retryWrites=true&w=majority',
-    test: 'mongodb+srv://wakoigi:<password>@cluster0.oyjbt.mongodb.net/?retryWrites=true&w=majority',
+    production: 'mongodb+srv://wakoigi:C0lombus01@@cluster0.oyjbt.mongodb.net/?retryWrites=true&w=majority',
+    development: 'mongodb+srv://wakoigi:C0lombus01@@cluster0.oyjbt.mongodb.net/?retryWrites=true&w=majority',
+    test: 'mongodb+srv://wakoigi:C0lombus01@@cluster0.oyjbt.mongodb.net/?retryWrites=true&w=majority',
 }
 module.exports = config;
